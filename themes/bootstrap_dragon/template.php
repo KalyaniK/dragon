53b3a3763c4bb5393ece94fdeea2f22035eea4f2
Jing <hejing.g@gmail.com>
2015-04-19 19:05:50 +0800

    DA-38: Initial version of the mobile friendly menu

--- themes/bootstrap_dragon/template.php
+++ themes/bootstrap_dragon/template.php
@@ -8,12 +8,12 @@
  * Preprocess variables for page template.
  */
 function bootstrap_dragon_preprocess_page(&$vars) {
-	if($vars['page']['header_top_left'] && $vars['page']['header_top_right']) { 
-		$vars['header_top_left_grid_class'] = 'col-md-6';
+	if($vars['page']['header_top_left'] && $vars['page']['header_top_right']) {
+		$vars['header_top_left_grid_class'] = 'col-md-7';
 		$vars['header_top_right_grid_class'] = 'col-md-2';
 	} elseif ($vars['page']['header_top_right'] || $vars['page']['header_top_left']) {
 		$vars['header_top_left_grid_class'] = 'col-md-5';
-		$vars['header_top_right_grid_class'] = 'col-md-12';		
+		$vars['header_top_right_grid_class'] = 'col-md-12';
 	}
   // Set the main grid class to full page
   $vars['main_grid_class'] = '';
