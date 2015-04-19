15d6cf25d916d514e3e6ca069ff7a151a5dbdbb9
lang <lang.wu.jane@gmail.com>
2015-04-19 18:29:02 +0800

    DA-34: Update the Images setting.

---
+++ modules/dragon_custom/dragon_front_page/js/dragon_front_page.js
@@ -0,0 +1,23 @@
+// $Id:: qad_ppa_select.js 2633 2014-03-26 02:34:39Z mjz                       $
+
+//jQuery wrapper
+(function($) {
+  Drupal.behaviors.auto_adjust_silder_height = {
+    attach: function(context, setting) {
+      function resize_images() {
+      	var images = $('.flexslider img');
+        var browser_width = $(window).width(),
+            browser_height = $(window).height() - $('#toolbar').height() - $('#header-top').height() - $('#main-navigation').height();
+            
+        if( browser_height/1080 >= browser_width/1920) {
+          images.height(browser_height).width('auto');
+        } 
+        else{
+          images.width(browser_width).width('auto');
+        }
+      }
+      $(window).bind('resize', function() { resize_images(); });
+      $( document ).ready(resize_images);
+    }
+  };
+})(jQuery);
